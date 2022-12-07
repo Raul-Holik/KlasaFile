b318fce33e42bc9bb0b97c70369d5251031073f0
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2022-12-07 09:26:36 +0100

    Add files via upload

---
+++ KlasaFile/KlasaFile/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace KlasaFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite ime. ");
+            string ime = Console.ReadLine();
+
+            string datoteka = "ime.txt";
+
+            if (File.Exists(datoteka)) 
+            {
+                if(!Directory.Exists("backup"))
+                {
+                    Directory.CreateDirectory("backup");
+                }
+                File.Copy(datoteka, "backup\\ime_" + DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss") + ".txt");
+                File.Delete(datoteka);
+
+            }
+            File.WriteAllText(datoteka, ime);
+            Console.ReadKey();  
+        }
+    }
+}
